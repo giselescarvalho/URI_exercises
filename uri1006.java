e5a53c2592979cb038d7896b0ce18c42f14fa2db
unknown <giseledasilvac@gmail.com>
2020-10-25 16:49:47 -0300

    uri 1006

---
+++ uri1006.java
@@ -0,0 +1,36 @@
+/*Leia 3 valores, no caso, variáveis A, B e C, que são as três notas de um aluno. A seguir, calcule a 
+ * média do aluno, sabendo que a nota A tem peso 2, a nota B tem peso 3 e a nota C tem peso 5. Considere que
+ *  cada nota pode ir de 0 até 10.0, sempre com uma casa decimal.
+
+Entrada
+O arquivo de entrada contém 3 valores com uma casa decimal, de dupla precisão (double).
+
+Saída
+Imprima a mensagem "MEDIA" e a média do aluno conforme exemplo abaixo, com 1 dígito após o ponto decimal e 
+com um espaço em branco antes e depois da igualdade. Assim como todos os problemas, não esqueça de imprimir 
+o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
+*/
+package uri;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class uri1006 {
+	public static void main(String[] args) throws IOException 
+	{
+	
+	Scanner ler = new Scanner(System.in);
+	double a,b,c,media;
+	
+	a = ler.nextDouble();
+	b = ler.nextDouble();
+	c = ler.nextDouble();
+	
+	media = ((a*2)+(b*3)+(c*5))/10;
+	
+	String med = String.format("%.1f", media);
+    System.out.println("MEDIA = " + med);
+	
+	
+	}
+}
