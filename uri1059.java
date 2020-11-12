609de4a03c4c283e3ebc8ef9d20e4e21c8ca797d
unknown <giseledasilvac@gmail.com>
2020-11-12 16:20:15 -0300

    uri 1059

---
+++ uri1059.java
@@ -0,0 +1,13 @@
+package uri;
+import java.io.IOException;
+public class uri1059 {
+	public static void main(String[] args) throws IOException {
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) System.out.println(i);
+
+            }
+
+        }
+ 
+}
